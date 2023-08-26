1839048edbc2a08e2a9d928c397bd81074b597a4
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-08-26 16:16:01 +0000

    Deploying to gh-pages from @ AnnaSosik/goit-js-hw-09@817286c1ecc0d164d32b9ccb24b3293337297ba2 🚀

---
+++ 01-color-switcher.36176a85.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(()=>{timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(()=>{clearInterval(timerId),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.36176a85.js.map
