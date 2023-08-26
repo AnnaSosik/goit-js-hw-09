817286c1ecc0d164d32b9ccb24b3293337297ba2
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-08-26 18:15:38 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+}
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled= false;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+});
